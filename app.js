7bc5618d5768f246df315d28e51dc420b166c1a7
Analin Flores <foglzerika@gmail.com>
2022-06-29 11:31:48 -0500

    feat| adding route add-product, product and body parser npm package

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000
+//declare a middleware to parse incoming request
+app.use(bodyParser.urlencoded({extended : false}));
+
 //middleware
 //called next function
-app.use((req,res,next) =>{
+app.use('/',(req,res,next) =>{
   console.log("Middleware");
   next();
-})
-app.use((req,res) =>{
-  console.log("2 part");
-  res.send("<h1>Hello world :) </h1>");
-})
+});
+
+app.use('/add-product',(req,res) =>{
+  res.send('<form action="/product" method="POST"><label for="product">Product:</label><input type="text" name="product"><label for="price">Price:</label><input type="number" name="price"><button type="submit">Add product</button></form>');
+});
+
+app.use('/product' , (req,res)=>{
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/' , (req,res)=>{
+  res.send('<h2>Product added</h2>')
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
