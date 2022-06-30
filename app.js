59a32c3de0b093c2e5b049f2114f1bf3aefcdcf4
Analin Flores <foglzerika@gmail.com>
2022-06-29 21:50:24 -0500

    feat | adding filtering paths

--- app.js
+++ app.js
@@ -14,8 +14,15 @@ app.use('/',(req,res,next) =>{
   console.log("Middleware");
   next();
 });
-app.use(adminRoutes);
+//routes
+//adding filtering paths
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+//default route
+app.use('*', ((req, res) => {
+  res.status(404).send('<h1>404 PAGE</h1>')
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+//route - admin/add-product - GET
 
 router.get('/add-product',(req,res) =>{
-  res.send('<form action="/product" method="POST"><label for="product">Product:</label><input type="text" name="product"><label for="price">Price:</label><input type="number" name="price"><button type="submit">Add product</button></form>');
+  res.send('<form action="/admin/product" method="POST"><label for="product">Product:</label><input type="text" name="product"><label for="price">Price:</label><input type="number" name="price"><button type="submit">Add product</button></form>');
 });
 
+// /admin/product - POST
 router.post('/product' , (req,res)=>{
   console.log(req.body);
   res.redirect('/');
