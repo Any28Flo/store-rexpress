b31dc82b19db8a48acb5c277703a174f5b0c3066
Analin Flores <foglzerika@gmail.com>
2022-06-29 22:08:54 -0500

    feat | adding views

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 //route - admin/add-product - GET
 
 router.get('/add-product',(req,res) =>{
-  res.send('<form action="/admin/product" method="POST"><label for="product">Product:</label><input type="text" name="product"><label for="price">Price:</label><input type="number" name="price"><button type="submit">Add product</button></form>');
+  res.send('');
 });
 
 // /admin/product - POST
